b20c006dac9a6ebcc5377f5142fd1e380e8139a5
p.watras <p.watras@dgmonitor.pl>
2020-11-25 19:12:01 +0100

    test for buffer-output

--- test/test.js
+++ test/test.js
@@ -8,6 +8,15 @@ trimImage(`images/inner/test.png`, `out/inner/test.png`, (err) => {
     }
 });
 
+trimImage(`images/inner/test.png`, null,(err,buf) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      fs.writeFileSync(`out/inner/buf-test.png`,buf);
+    }
+});
+
 trimImage(`images/inner/test.png`, `out/inner/test1.png`, { top: false }, (err) => {
     if (err) {
       console.log(err);
